7a519358ebe620370916d4f1cef1ee463387332f
grupotriplaaposta <135553207+grupotriplaaposta@users.noreply.github.com>
2023-06-09 16:15:06 -0300

    Update acess.js

--- acess.js
+++ acess.js
@@ -3,7 +3,7 @@ function pegaval(event){
     var senha  = "TRIPLATESTE";
     var acesso = document.getElementById("acess").value;
 
-    var senhasValidas = ["231912", "295649", "643632", "869122", "246810"];
+    var senhasValidas = ["231912", "295649", "643632", "869122", "246810", "120891", "Lemes01"];
 
     // Verificar se a senha está na lista de senhas válidas
     if (senhasValidas.includes(acesso)) {
